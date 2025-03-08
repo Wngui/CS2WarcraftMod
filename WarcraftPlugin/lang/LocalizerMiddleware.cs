67a0a687e8de2643a8a6ba0a64f610f616c6a4ea
Oliver Vestergaard <Lhrbozz@msn.com>
2025-03-08 08:52:10 +0100

    danish language, fixed bugs

--- WarcraftPlugin/lang/LocalizerMiddleware.cs
+++ WarcraftPlugin/lang/LocalizerMiddleware.cs
@@ -22,7 +22,7 @@ namespace WarcraftPlugin.lang
 
             // Process the localizer strings
             var localizedStrings = localizer.GetAllStrings()
-                .Select(ls => new LocalizedString(ls.Name, ReplaceChatColors(ls.Value, chatColors)))
+                .Select(ls => new LocalizedString(ls.Name.ToLower(), ReplaceChatColors(ls.Value.ToLower(), chatColors)))
                 .Concat(customHeroLocalizerStrings)
                 .ToList();
 
@@ -32,36 +32,43 @@ namespace WarcraftPlugin.lang
         private static List<LocalizedString> LoadCustomHeroLocalizations(string moduleDirectory, Dictionary<string, string> chatColors)
         {
             var searchPattern = $"*.{CultureInfo.CurrentUICulture.TwoLetterISOLanguageName}*.json";
+            var fallbackSearchPattern = "*.en*.json";
 
             var customHeroLocalizations = Directory.EnumerateFiles(Path.Combine(moduleDirectory, "lang"), searchPattern);
+            var fallbackLocalizations = Directory.EnumerateFiles(Path.Combine(moduleDirectory, "lang"), fallbackSearchPattern);
 
             // Use a thread-safe collection for parallel processing
             var concurrentLocalizerStrings = new ConcurrentBag<LocalizedString>();
 
-            Parallel.ForEach(customHeroLocalizations, file =>
+            var jsonOptions = new JsonSerializerOptions { AllowTrailingCommas = true };
+
+            Parallel.ForEach(customHeroLocalizations.Concat(fallbackLocalizations), file =>
             {
                 var jsonContent = File.ReadAllText(file);
-                var customHeroLocalizations = JsonSerializer.Deserialize<Dictionary<string, string>>(jsonContent);
+                var customHeroLocalizations = JsonSerializer.Deserialize<Dictionary<string, string>>(jsonContent, jsonOptions);
 
                 if (customHeroLocalizations != null)
                 {
                     foreach (var localization in customHeroLocalizations)
                     {
-                        concurrentLocalizerStrings.Add(new LocalizedString(localization.Key, ReplaceChatColors(localization.Value, chatColors)));
+                        concurrentLocalizerStrings.Add(new LocalizedString(localization.Key, ReplaceChatColors(localization.Value.ToLower(), chatColors), false, searchedLocation: file));
                     }
                 }
             });
 
-            // Add the parallel-processed strings to the main list
-            var localizerStrings = new List<LocalizedString>();
-            localizerStrings.AddRange(concurrentLocalizerStrings);
-            return localizerStrings;
+            // Use English as fallback
+            var uniqueLocalizerStrings = concurrentLocalizerStrings
+                .GroupBy(ls => ls.Name)
+                .Select(g => g.FirstOrDefault(ls => !ls.SearchedLocation.Contains(".en.")) ?? g.First())
+                .ToList();
+
+            return uniqueLocalizerStrings;
         }
 
         private static Dictionary<string, string> GetChatColors()
         {
             return typeof(ChatColors).GetProperties()
-                .ToDictionary(prop => prop.Name, prop => prop.GetValue(null)?.ToString() ?? string.Empty, StringComparer.InvariantCultureIgnoreCase);
+                .ToDictionary(prop => prop.Name.ToLower(), prop => prop.GetValue(null)?.ToString() ?? string.Empty, StringComparer.InvariantCultureIgnoreCase);
         }
 
         private static readonly Regex ChatColorRegex = new Regex(@"{(\D+?)}", RegexOptions.IgnoreCase | RegexOptions.Compiled);
@@ -70,7 +77,7 @@ namespace WarcraftPlugin.lang
         {
             return ChatColorRegex.Replace(input, match =>
             {
-                var key = match.Groups[1].Value;
+                var key = match.Groups[1].Value.ToLower();
                 return chatColors.TryGetValue(key, out var value) ? value : "{UNKNOWN-COLOR}";
             });
         }
@@ -80,10 +87,10 @@ namespace WarcraftPlugin.lang
     {
         private readonly List<LocalizedString> _localizedStrings = localizedStrings;
 
-        public LocalizedString this[string name] => _localizedStrings.FirstOrDefault(ls => ls.Name == name) ?? new LocalizedString(name, name);
+        public LocalizedString this[string name] => _localizedStrings.FirstOrDefault(ls => ls.Name == name.ToLower()) ?? new LocalizedString(name.ToLower(), name.ToLower());
 
         public LocalizedString this[string name, params object[] arguments] =>
-            new(name, string.Format(_localizedStrings.FirstOrDefault(ls => ls.Name == name)?.Value ?? name, arguments));
+            new(name.ToLower(), string.Format(_localizedStrings.FirstOrDefault(ls => ls.Name == name.ToLower())?.Value ?? name.ToLower(), arguments));
 
         public IEnumerable<LocalizedString> GetAllStrings(bool includeParentCultures) => _localizedStrings;
 
