45d556e32895df0b00b887229d3533b6aea303c0
Oliver Vestergaard <Lhrbozz@msn.com>
2025-07-05 11:51:13 +0200

    move stun effect to shared

--- WarcraftPlugin/Helpers/Warcraft.cs
+++ WarcraftPlugin/Helpers/Warcraft.cs
@@ -425,52 +425,6 @@ namespace WarcraftPlugin.Helpers
         }
 
         /// <summary>
-        /// Stuns the player by setting their movement speed to 0 for a duration.
-        /// </summary>
-        /// <param name="player">The player controller to stun.</param>
-        /// <param name="duration">How long the stun should last in seconds.</param>
-        public static void Stun(this CCSPlayerController player, float duration, CCSPlayerController attacker = null, string abilityName = null)
-        {
-            if (!player.IsAlive()) return;
-
-            new StunEffect(player, attacker, abilityName, duration).Start();
-        }
-
-        private class StunEffect(CCSPlayerController owner, CCSPlayerController attacker, string abilityName, float duration) : WarcraftEffect(owner, duration)
-        {
-            private float _originalSpeed;
-            private float _originalModifier;
-            private readonly CCSPlayerController _attacker = attacker;
-            private readonly string _abilityName = abilityName;
-
-            public override void OnStart()
-            {
-                if (!Owner.IsAlive()) return;
-                var pawn = Owner.PlayerPawn.Value;
-                _originalSpeed = pawn.MovementServices.Maxspeed;
-                _originalModifier = pawn.VelocityModifier;
-                pawn.MovementServices.Maxspeed = 0;
-                pawn.VelocityModifier = 0;
-
-                if (!string.IsNullOrEmpty(_abilityName))
-                {
-                    Owner.PrintToChat($" {ChatColors.Red}Stunned by {ChatColors.Green}{_abilityName}");
-                    _attacker?.PrintToChat($" {ChatColors.Green}{_abilityName}{ChatColors.Default} stunned {Owner.GetRealPlayerName()}");
-                }
-            }
-
-            public override void OnTick() { }
-
-            public override void OnFinish()
-            {
-                if (!Owner.IsAlive()) return;
-                var pawn = Owner.PlayerPawn.Value;
-                pawn.MovementServices.Maxspeed = _originalSpeed;
-                pawn.VelocityModifier = _originalModifier;
-            }
-        }
-
-        /// <summary>
         /// Blinds the player by displaying a fade effect with the specified duration and color.
         /// </summary>
         /// <param name="player">The player controller to blind.</param>
